e798ddc4663e86d38230c7f30eacdb65e34f534c
Melanie Burger <melanie.brgr@gmail.com>
2017-01-21 13:33:21 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      planets: []
+    }
+    this.getAll_SWAPI = this.getAll_SWAPI.bind(this);
+  }
+
+  getAll_SWAPI(stateName) {
+    // response from SWAPI API is multi-part pages
+    // this function calls itself recursively to get all pages
+    // then finally sets the state will all data
+    // TODO: right now the stateName argument must exist on the state object and must also be the API attribute requested; this should probably be more flexible
+
+    let data = [];
+    const that = this;
+    (function get(url) {
+      let nextUrl = arguments.length ? url : `http://swapi.co/api/${stateName}/`;
+      $.get(nextUrl).then(response => {
+        data = data.concat(response.results);
+        response.next ? get(response.next) : that.setState({ [stateName]: data });
+      });
+    })();
+  }
+
+  componentDidMount() {
+    this.getAll_SWAPI('planets');
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>The lonely planets guide to Star Wars</h1>
+        </div>
+        <p className="App-intro">
+          Tips and helpful information for the intergalactic planet hopper.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
