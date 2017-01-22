00cc3dcf6c8cc2248c3e2e72c07da21f38acd436
Melanie Burger <melanie.brgr@gmail.com>
2017-01-22 18:09:41 -0500

    include key for PlanetCard list

--- src/containers/PlanetCards-container.js
+++ src/containers/PlanetCards-container.js
@@ -5,7 +5,7 @@ import PlanetCard from '../components/PlanetCard/PlanetCard';
 export default function PlanetCardsContainer({planets}) {
   return (
     <main>
-      {planets.map( planet => <PlanetCard planet={planet} />)}
+      {planets.map( (planet, i) => <PlanetCard planet={planet} key={i} />)}
     </main>
   );
 }
