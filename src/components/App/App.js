c57321052c07ce5bf286d50816daf3cab931bf77
Melanie Burger <melanie.brgr@gmail.com>
2017-01-22 13:29:16 -0500

    make App a dumb component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from '../Header/Header';
 import Nav from '../Nav/Nav';
@@ -6,27 +6,22 @@ import PlanetCardsContainer from '../../containers/PlanetCards-container';
 
 import './App.css';
 
-export default class App extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    const childrenWithProps = React.Children.map(this.props.children,
-      child => {
-        if (child.type === PlanetCardsContainer) {
-          return React.cloneElement(child, { planets: this.props.planets });
-        }
-        else 
-          return child;
+export default function App({planets, children}) {
+  const childrenWithProps = React.Children.map(children,
+    child => {
+      if (child.type === PlanetCardsContainer) {
+        return React.cloneElement(child, { planets: planets });
       }
-    );
+      else 
+        return child;
+    }
+  );
 
-    return (
-      <div className="App">
-        <Header />
-        <Nav planets={this.props.planets}/>
-        {childrenWithProps}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header />
+      <Nav planets={planets}/>
+      {childrenWithProps}
+    </div>
+  );  
 }
