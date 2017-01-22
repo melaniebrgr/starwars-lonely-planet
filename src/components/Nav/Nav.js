fc8e9c2ead2c5d659dba692094026b3766b787aa
Melanie Burger <melanie.brgr@gmail.com>
2017-01-22 16:57:57 -0500

    add key prop to NavList items

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ import NavList from './NavList';
 export default function Nav({planets}) {
   return (
     <ul>
-      {planets.map(planet => <NavList planet={planet}/>)}
+      {planets.map((planet, i) => <NavList planet={planet} key={i} />)}
     </ul>
   );
 }
