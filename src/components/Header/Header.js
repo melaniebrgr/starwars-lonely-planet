3737fdb56be3cf1e11a9e620504e071f2d6846e8
Melanie Burger <melanie.brgr@gmail.com>
2017-01-22 18:32:02 -0500

    link to all planets in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import './Header.css';
 
 export default function Header() {
-  return(
+  return (
     <div className="Header">
       <h1 className="Header__title">{`The Lonely Planets' guide to Star Wars`}</h1>
       <p className="Header__intro">
-        Advice and information for the Star Wars universe traveller.
-      </p>          
+        Advice and information for the Star Wars universe traveller. <Link to={`/planets`}>See all planets</Link> 
+      </p>
     </div>
   )
 }
