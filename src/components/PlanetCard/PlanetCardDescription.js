1323e00e43eaf799bc394b8e1dbbd72691e67698
Melanie Burger <melanie.brgr@gmail.com>
2017-01-22 20:07:52 -0500

    delete extra space in planet description

--- src/components/PlanetCard/PlanetCardDescription.js
+++ src/components/PlanetCard/PlanetCardDescription.js
@@ -8,7 +8,7 @@ export default function PlanetCardDescription({
   return (
     <div className="PlanetCard__Description">
       <h4>Description</h4>
-      <p>{`The planet ${planet.name} has a ${planet.climate.toLowerCase()} climate. It's ${diameterAdjective(planet.diameter)} Earth, with ${diameterProportion(planet.diameter)} it's diameter. ${planet.name}'s population is ${populationAdjective(planet.population)} Earth's with ${numberWithCommas(planet.population)} sentient lifeforms, which is supported by ${planet.surface_water} % surface water coverage.`}</p>
+      <p>{`The planet ${planet.name} has a ${planet.climate.toLowerCase()} climate. It's ${diameterAdjective(planet.diameter)} Earth, with ${diameterProportion(planet.diameter)} it's diameter. ${planet.name}'s population is ${populationAdjective(planet.population)} Earth's with ${numberWithCommas(planet.population)} sentient lifeforms, which is supported by ${planet.surface_water}% surface water coverage.`}</p>
     </div>
   );
 }
